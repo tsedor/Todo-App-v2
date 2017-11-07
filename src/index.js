fa167ad95819d16f74c923b3b626aca3ed5982d9
Tomek <itsme199@gmail.com>
2017-11-07 16:41:19 +0100

    Added redux store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import TodoApp from './components/TodoApp';
 
+const store = createStore(reducers);
+
 ReactDOM.render(
-  <TodoApp />,
+  <Provider store={store}>
+    <MuiThemeProvider>
+      <TodoApp />
+    </MuiThemeProvider>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import todos from './todos';
+
+const reducers = combineReducers(todos);
+
+export default reducers;
